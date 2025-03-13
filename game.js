1ce80cb87e87658249d67ac88a4ea25de1002f51
Man <nguyenman025@gmail.com>
2025-03-13 14:47:13 -0400

    add board module

--- game.js
+++ game.js
@@ -1,4 +1,36 @@
-import Board from './modules/board';
-
-// Initialize the board
-console.log(Board.isBoardFull());
+const Board = (() => {
+    const cells = [...document.getElementsByClassName("cell")];
+    const isBoardFull = () => {
+        return cells.every(cell => cell.textContent!== "");
+    }
+    const hasWinner = () => {
+        // Check rows, columns, and diagonals for a winner
+        for (let i = 0; i < 3; i++) {
+            // checks rows
+            if (cells[i * 3].textContent !== '' && cells[i * 3].textContent === cells[i * 3 + 1].textContent && cells[i * 3].textContent === cells[i * 3 + 2].textContent) {
+                return true;
+            }
+            // checks columns
+            if (cells[i].textContent !== '' & cells[i].textContent === cells[i + 3].textContent && cells[i].textContent === cells[i + 6].textContent) {
+                return true;
+            }
+        }
+        // checks diagonals
+        if (cells[0].textContent !== '' && cells[0].textContent === cells[4].textContent && cells[0].textContent === cells[8].textContent) {
+            return true;
+        }
+        if (cells[2].textContent !== '' && cells[2].textContent === cells[4].textContent && cells[2].textContent === cells[6].textContent) {
+            return true;
+        }
+        return false;
+    }
+    const placeMarker = (cellNum, symbol) => {
+        if (cells[cellNum].textContent === "") {
+            cells[cellNum].textContent = symbol;
+            return true;
+        }
+        return false;
+    }
+    return {isBoardFull, hasWinner, placeMarker}
+})();
+console.log(Board.hasWinner());
